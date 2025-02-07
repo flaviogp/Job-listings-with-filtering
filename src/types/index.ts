eccd17760a5c5bc75c606b8c73a1bb60ad0e66af
FlavioGP <flavio.gpinheiro@hotmail.com>
2025-02-07 09:10:58 -0300

    chore: initial configs

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type DataType = {
+    company: string;
+    logo: string;
+    isNew: boolean;
+    featured: boolean;
+    position: string;
+    postedAt: string;
+    contract: string;
+    location: string;
+    role: string;
+    level: string;
+    languages: string[];
+    tools: string[];
+}
